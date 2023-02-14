7daa30a00b5a752794f984ed300e73288acaac7d
Nahimana <mnahim024@gmail.com>
2023-02-14 20:37:20 +0100

    ajout des boutons filtres

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,21 +1,49 @@
-async function afficherProjets() {
-  const projets = await fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((data) => data);
+const projets = await fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => data);
 
-  for (let projet of projets) {
+function afficherProjets(projets) {
+  for (let i = 0; i < projets.length; i++) {
+    const image = document.createElement("img");
+    image.src = projets[i].imageUrl;
+    const titre = document.createElement("figcaption");
+    titre.innerText = projets[i].title;
+
     const gallery = document.querySelector(".gallery");
     const figure = document.createElement("figure");
-
-    const image = document.createElement("img");
-    image.src = projet.imageUrl;
-    const titre = document.createElement("figcaption");
-    titre.innerText = projet.title;
-
     gallery.appendChild(figure);
     figure.appendChild(image);
     figure.appendChild(titre);
   }
 }
+afficherProjets(projets);
+const btnTous = document.querySelector(".Tous");
+btnTous.addEventListener("click", () => {
+  document.querySelector(".gallery").innerHTML = "";
+  afficherProjets(projets);
+});
 
-afficherProjets();
+const btnObjets = document.querySelector(".Objets");
+btnObjets.addEventListener("click", () => {
+  const objets = projets.filter((projet) => {
+    return projet.category.id == 1;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  afficherProjets(objets);
+});
+const btnAppartements = document.querySelector(".Appartements");
+btnAppartements.addEventListener("click", () => {
+  const appartements = projets.filter((projet) => {
+    return projet.category.id == 2;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  afficherProjets(appartements);
+});
+const btnHotelRestaurant = document.querySelector(".Hotels-restaurants");
+btnHotelRestaurant.addEventListener("click", () => {
+  const hotelRestaurant = projets.filter((projet) => {
+    return projet.category.id == 3;
+  });
+  document.querySelector(".gallery").innerHTML = "";
+  afficherProjets(hotelRestaurant);
+});
