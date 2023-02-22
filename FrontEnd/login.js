1640e8ffca2c809452b47b98c25548ddaf640395
Nahimana <mnahim024@gmail.com>
2023-02-22 13:41:08 +0100

    recupération du token

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -2,20 +2,22 @@ const userSophie = {
   email: "sophie.bluel@test.tld",
   password: "S0phie",
 };
-function login(objet) {
-  const autorisation = fetch("http://localhost:5678/api/users/login", {
+async function login(objet) {
+  const token = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: { "content-type": "application/json" },
     body: JSON.stringify(objet),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data.token));
+    .then((data) => data.token);
+  console.log(token);
 }
+const form = document.querySelector(".login-section");
 const adressMail = document.getElementById("email");
 const password = document.getElementById("password");
 const seConnecter = document.getElementById("submit");
 
-seConnecter.addEventListener("click", (event) => {
+form.addEventListener("submit", (event) => {
   console.log("click");
   event.preventDefault();
 
@@ -24,9 +26,12 @@ seConnecter.addEventListener("click", (event) => {
     password: password.value,
   };
   if (user.email == userSophie.email && user.password == userSophie.password) {
-    console.log("ok");
     login(user);
   } else {
-    console.log("vos id sont incorrectes");
+    const error = document.querySelector(".error");
+    const messageError = document.createElement("p");
+    messageError.innerText = "Erreur dans l’identifiant ou le mot de passe";
+    messageError.style.color = "red";
+    error.appendChild(messageError);
   }
 });
